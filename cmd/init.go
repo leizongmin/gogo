00c2f00bb88bf16a3d873f5ad610df51361fcab8
Zongmin Lei <leizongmin@gmail.com>
2017-09-12 13:27:01 +0800

    fix init

--- cmd/init.go
+++ cmd/init.go
@@ -27,15 +27,15 @@ func Init(args []string) {
 	workspace := pkg.Dir.Workspace
 	pwd := pkg.Dir.Pwd
 
-	exec("mkdir", "-p", filepath.Join(workspace, "src", filepath.Dir(pkg.Package)))
-	exec("ln", "-s", pwd, filepath.Join(workspace, "src", pkg.Package))
+	exec(pwd, "mkdir", "-p", filepath.Join(workspace, "src", filepath.Dir(pkg.Package)))
+	exec(pwd, "ln", "-s", pwd, filepath.Join(workspace, "src", pkg.Package))
 
-	exec("mkdir", "-p", filepath.Join(pwd, "vendor"))
-	exec("mkdir", "-p", filepath.Join(workspace, "vendor"))
-	exec("ln", "-s", filepath.Join(pwd, "vendor"), filepath.Join(workspace, "vendor", "src"))
+	exec(pwd, "mkdir", "-p", filepath.Join(pwd, "vendor"))
+	exec(pwd, "mkdir", "-p", filepath.Join(workspace, "vendor"))
+	exec(pwd, "ln", "-s", filepath.Join(pwd, "vendor"), filepath.Join(workspace, "vendor", "src"))
 
-	exec("mkdir", "-p", filepath.Join(workspace, "vendor", "pkg"))
-	exec("ln", "-s", filepath.Join(workspace, "vendor", "pkg"), filepath.Join(workspace, "pkg"))
+	exec(pwd, "mkdir", "-p", filepath.Join(workspace, "vendor", "pkg"))
+	exec(pwd, "ln", "-s", filepath.Join(workspace, "vendor", "pkg"), filepath.Join(workspace, "pkg"))
 
 	fmt.Println("\nOK")
 
